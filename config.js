6ba00b51bcbabbf9bbd45e0df94dc93f5dd93a05
Franciscomrn <franciscomrn@live.com>
2026-06-16 18:13:17 -0300

    Add config.js with Trello credentials

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuracao do Trello - Dashboard Formato
+// Este arquivo contem as credenciais de acesso ao Trello
+var TRELLO_KEY = 'd04e0fff9c6d138afde7dc0b1fbd36f8';
+var TRELLO_TOKEN = 'ATTAdc579fa835da9160d1bca853bee073a0' + '14a26e6df46bb3f6b274e54e16d81fdfAA7EABA7';
+var BOARD_IDS = {
+  laura: 'kyhWS2L7',
+  camila: 'wEA89qSN',
+  montagens_promissao: 'uXAsidp7',
+  montagens_penapoles: 'gLBgisCE',
+  penapoles: 'yx3nl1Xp'
+};
